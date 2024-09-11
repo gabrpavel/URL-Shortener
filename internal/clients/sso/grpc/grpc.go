545b2e64a61e3548812d8e1a906187eef813ca53
gabrpavel <gabrpavel@gmail.com>
2024-09-11 14:03:08 +0300

    add grpc client

---
+++ internal/clients/sso/grpc/grpc.go
@@ -0,0 +1,74 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	ssov1 "github.com/gabrpavel/protos/gen/go/sso"
+	grpclog "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	grpcretry "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/retry"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"log/slog"
+	"time"
+)
+
+type Client struct {
+	api ssov1.AuthClient
+	log *slog.Logger
+}
+
+func New(
+	ctx context.Context,
+	log *slog.Logger,
+	addr string,
+	timeout time.Duration,
+	retriesCount int,
+) (*Client, error) {
+	const op = "grpc.New"
+
+	retryOptions := []grpcretry.CallOption{
+		grpcretry.WithCodes(codes.NotFound, codes.Aborted, codes.DeadlineExceeded),
+		grpcretry.WithMax(uint(retriesCount)),
+		grpcretry.WithPerRetryTimeout(timeout),
+	}
+
+	logOptions := []grpclog.Option{
+		grpclog.WithLogOnEvents(grpclog.PayloadReceived, grpclog.PayloadSent),
+	}
+
+	cc, err := grpc.DialContext(ctx, addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithChainUnaryInterceptor(
+			grpclog.UnaryClientInterceptor(InterceptorLogger(log), logOptions...),
+			grpcretry.UnaryClientInterceptor(retryOptions...),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Client{
+		api: ssov1.NewAuthClient(cc),
+	}, nil
+}
+
+func (c *Client) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	const op = "grpc.IsAdmin"
+
+	resp, err := c.api.IsAdmin(ctx, &ssov1.IsAdminRequest{
+		UserId: userID,
+	})
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return resp.IsAdmin, nil
+}
+
+// InterceptorLogger adapts slog.Logger to interceptor logger.
+func InterceptorLogger(log *slog.Logger) grpclog.Logger {
+	return grpclog.LoggerFunc(func(ctx context.Context, lvl grpclog.Level, msg string, args ...any) {
+		log.Log(ctx, slog.Level(lvl), msg, args...)
+	})
+}
